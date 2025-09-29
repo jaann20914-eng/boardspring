715034989f1878a3685d695db290f9c4fa1ef88e
pwrmin <pwrmin@naver.com>
2025-09-29 11:12:58 +0900

    start

---
+++ src/test/java/com/keud/boardproject/BoardprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keud.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
